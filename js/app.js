ca9090cd0d2b8fc261cd2e69839f2ace6a2d288e
Liza <100815059+ap-liza@users.noreply.github.com>
2023-04-26 10:43:14 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+let screen = document.querySelector('.screen');
+let button = document.querySelectorAll('.btn');
+let clear = document.querySelector('.btn-red');
+let equal = document.querySelector('.btn-green');
+
+
+
+
+button.forEach(function(button){
+    button.addEventListener('click', function(e){
+        let value = e.target.dataset.num;
+        screen.value +=value
+    })
+})
+
+equal.addEventListener('click', function(e){
+    if(screen.value === ''){
+        screen.value = '';
+    }else{
+        let answer = eval(screen.value);
+        screen.value = answer;
+    }
+})
+
+clear.addEventListener('click', function(e){
+    screen.value = " ";
+})
